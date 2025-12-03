a2bd5d5dc589797417d79c1d94a462153e100a13
NSGolova <golova@golova.dev>
2025-12-03 21:16:44 +0000

    Fixed parts pp not being saved for players

--- PlayersRefresh.cs
+++ PlayersRefresh.cs
@@ -97,7 +97,7 @@ namespace portaBLe
                 }
             }
             await dbContext.BulkUpdateAsync(scoreUpdates, options => options.ColumnInputExpression = c => new { c.Weight });
-            await dbContext.BulkUpdateAsync(playerUpdates, options => options.ColumnInputExpression = c => new { c.Rank, c.Pp, c.TopPp, c.RankedPlayCount, c.CountryRank });
+            await dbContext.BulkUpdateAsync(playerUpdates, options => options.ColumnInputExpression = c => new { c.Rank, c.Pp, c.TopPp, c.RankedPlayCount, c.CountryRank, c.AccPp, c.TechPp, c.PassPp });
         }
     }
 
